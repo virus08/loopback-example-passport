0985814e83cfdfc081df8169c2bd3273d06dd5c9
Wanchai <Wanchai@WANCHAI.tvs>
2017-10-17 17:32:24 +0700

    add client content

---
+++ client/js/app01.vue
@@ -0,0 +1,16 @@
+<template>
+	<button>Get JSON data</button>
+	<div></div>
+</template>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+	$(document).ready(function(){
+	    $("button").click(function(){
+	        $.getJSON("demo_ajax_json.js", function(result){
+	            $.each(result, function(i, field){
+	                $("div").append(field + " ");
+	            });
+	        });
+	    });
+	});
+</script>
